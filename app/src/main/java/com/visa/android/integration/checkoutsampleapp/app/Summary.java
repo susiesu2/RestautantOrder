d1b41f90d653738cb3b7acfc5bf53805b9b2b2c4
CheetoZhang <jzhang@Jennys-MacBook-Pro.local>
2016-03-06 13:04:10 -0800

    First Init of NFC, VISA, Summary

---
+++ app/src/main/java/com/visa/android/integration/checkoutsampleapp/app/Summary.java
@@ -0,0 +1,104 @@
+package com.visa.android.integration.checkoutsampleapp.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class Summary extends Activity {
+    String tableNo;
+    public static double amount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+        TableLayout tl = (TableLayout) findViewById(R.id.tableLayout);
+        Resources res = getResources();
+        String[] food = res.getStringArray(R.array.food);
+        String[] price = res.getStringArray(R.array.price);
+        Bundle b = this.getIntent().getExtras();
+        String[] qty = b.getStringArray("qty");
+        //tableNo = b.getString("tableNo");
+        tableNo = "3";
+        Double sum = 0.0;
+        int length = res.getInteger(R.integer.length);
+        for (int i = 0; i < length; i++) {
+            if (Integer.parseInt(qty[i]) == 0) continue;
+
+            sum += Double.parseDouble(price[i]) * Integer.parseInt(qty[i]);
+
+            TableRow tr = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            tr.setLayoutParams(lp);
+
+            TextView tvLeft = new TextView(this);
+            tvLeft.setLayoutParams(lp);
+            tvLeft.setBackgroundColor(Color.LTGRAY);
+            tvLeft.setTextColor(Color.BLACK);
+            tvLeft.setGravity(Gravity.CENTER);
+            tvLeft.setText(food[i]);
+
+            TextView tvCenter = new TextView(this);
+            tvCenter.setLayoutParams(lp);
+            tvCenter.setBackgroundColor(Color.LTGRAY);
+            tvCenter.setTextColor(Color.BLACK);
+            tvCenter.setGravity(Gravity.CENTER);
+            tvCenter.setText(price[i]);
+
+            TextView tvRight = new TextView(this);
+            tvRight.setLayoutParams(lp);
+            tvRight.setBackgroundColor(Color.LTGRAY);
+            tvRight.setTextColor(Color.BLACK);
+            tvRight.setGravity(Gravity.CENTER);
+            tvRight.setText(qty[i]);
+
+            tr.addView(tvLeft);
+            tr.addView(tvCenter);
+            tr.addView(tvRight);
+
+            tl.addView(tr, lp);
+        }
+        TextView sub = (TextView) findViewById(R.id.subtotal);
+        TextView tax = (TextView) findViewById(R.id.tax);
+        TextView total = (TextView) findViewById(R.id.total);
+        sub.append(" $" + sum);
+        double taxValue = Math.round(sum * 0.0875 * 100.0) / 100.0;
+        tax.append(" $" + taxValue);
+        amount = sum + taxValue;
+        total.append(" $" + amount);
+        TextView table = (TextView) findViewById(R.id.table);
+
+        if (tableNo != null){
+            table.setText("You are at table " + tableNo);
+        } else {
+            table.setText(R.string.prompt);
+            /*
+            TimePicker timePicker = new TimePicker(this);
+            TimePicker.LayoutParams tlp= new TimePicker.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            timePicker.setLayoutParams(tlp);
+            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+            linearLayout.addView(linearLayout);
+            */
+        }
+
+        //int hour = timePicker.getHour();
+        //int min = timePicker.getMinute();
+
+    }
+    public void back(View view) {
+        Intent i = new Intent(this, TheMenu.class);
+        startActivity(i);
+    }
+
+    public void goToPay(View view) {
+        Intent i =new Intent(this,ConfigurePaymentActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/visa/android/integration/checkoutsampleapp/app/TheMenu.java
@@ -0,0 +1,56 @@
+package com.visa.android.integration.checkoutsampleapp.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Spinner;
+
+
+public class TheMenu extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_menu);
+
+    }
+
+    public void next(View view) {
+        String[] qty = new String[5];
+        int id = 0;
+        Spinner spinner1 = (Spinner) findViewById(R.id.Q1);
+        String q1 = spinner1.getSelectedItem().toString();
+        qty[id++] = q1;
+        Spinner spinner2 = (Spinner) findViewById(R.id.Q2);
+        String q2 = spinner2.getSelectedItem().toString();
+        qty[id++] = q2;
+        Spinner spinner3 = (Spinner) findViewById(R.id.Q3);
+        String q3 = spinner3.getSelectedItem().toString();
+        qty[id++] = q3;
+        Spinner spinner4 = (Spinner) findViewById(R.id.Q4);
+        String q4 = spinner4.getSelectedItem().toString();
+        qty[id++] = q4;
+        Spinner spinner5 = (Spinner) findViewById(R.id.Q5);
+        String q5 = spinner5.getSelectedItem().toString();
+        qty[id++] = q5;
+        Intent i = new Intent(getApplicationContext(), Summary.class);
+        Bundle b = new Bundle();
+        b.putStringArray("qty", qty);
+        i.putExtras(b);
+        startActivity(i);
+    }
+
+
+    public void back(View view) {
+        Intent i =new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+/*
+    public void goToSummary(View view) {
+        Intent i =new Intent(this,Summary.class);
+        startActivity(i);
+    }
+*/
+
+}
