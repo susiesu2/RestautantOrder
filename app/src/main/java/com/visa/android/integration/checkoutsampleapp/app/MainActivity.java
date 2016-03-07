5604f22a185ea2bb29b5990fb8ce749dc3aafc9c
whw87210 <hwang@scu.edu>
2016-03-06 17:13:24 -0800

    First Time Functional App

---
+++ app/src/main/java/com/visa/android/integration/checkoutsampleapp/app/MainActivity.java
@@ -0,0 +1,186 @@
+package com.visa.android.integration.checkoutsampleapp.app;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends FragmentActivity {
+
+
+
+    private static final String TAG = "MainActivity";
+
+
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurant_activity_main);
+
+
+
+        mTextView = (TextView) findViewById(R.id.tableNumberVar);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        }
+
+
+
+
+    }
+/*
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.login, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.login:
+                Intent loginIntent = new Intent(this, LoginFragment.class);
+                startActivity(loginIntent);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
+
+
+    public void onClick(View view) {
+        Intent i =new Intent(this,TheMenu.class);
+        startActivity(i);
+    }
+*/
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        Toast.makeText(this,"NFC intent received!", Toast.LENGTH_LONG).show();
+
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (parcelables != null && parcelables.length > 0) {
+
+            readTextFromMessage((NdefMessage)parcelables[0]);
+
+
+        }else{
+            Toast.makeText(this, "NO NDEF message found!", Toast.LENGTH_SHORT).show();
+        }
+
+        super.onNewIntent(intent);
+    }
+
+
+    @Override
+    protected void onResume() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 ,intent, 0);
+        IntentFilter[] intentFilter = new IntentFilter[]{};
+
+        mNfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+        super.onResume();
+
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+    }
+
+    @Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        mNfcAdapter.disableForegroundDispatch(this);
+
+        super.onPause();
+    }
+
+    private void readTextFromMessage (NdefMessage ndefMessage) {
+        NdefRecord[] ndefRecords = ndefMessage.getRecords();
+
+        if (ndefRecords != null && ndefRecords.length>0) {
+
+            NdefRecord ndefRecord = ndefRecords[1];
+
+            String tagContent = getTextFromNdefRecord(ndefRecord);
+
+            tableNo = tagContent;
+
+            mTextView.setText(tagContent);
+
+        }else{
+            Toast.makeText(this, "No NDEF records found!", Toast.LENGTH_LONG).show();
+
+        }
+
+
+    }
+
+    public static String tableNo = "";
+
+
+
+
+    private String getTextFromNdefRecord(NdefRecord ndefRecord) {
+        String tagContent = null;
+        try {
+            byte[] payload = ndefRecord.getPayload();
+            String textEncoding = "UTF-8";
+            int languageSize = payload[0] & 0063;
+            tagContent = new String(payload, languageSize + 1,
+                    payload.length - languageSize - 1, textEncoding);
+
+        } catch (UnsupportedEncodingException e) {
+            Log.e("getTextFromNdefRecord", e.getMessage(), e);
+        }
+        return tagContent;
+
+    }
+
+    public void goToMenu (View view){
+        Intent intent = new Intent (this, TheMenu.class);
+        startActivity(intent);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/visa/android/integration/checkoutsampleapp/app/PaymentSummaryActivity.java
@@ -0,0 +1,81 @@
+package com.visa.android.integration.checkoutsampleapp.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.visa.checkout.VisaLibrary;
+import com.visa.checkout.VisaPaymentSummary;
+
+import static com.visa.android.integration.checkoutsampleapp.app.ConfigureVisaPaymentInfo.VISA_CHECKOUT_REFERENCE_ID_KEY;
+import static com.visa.android.integration.checkoutsampleapp.app.ConfigureVisaPaymentInfo.VISA_CHECKOUT_RESULT_MSG_KEY;
+import static com.visa.android.integration.checkoutsampleapp.app.ConfigureVisaPaymentInfo.getSampleVisaPaymentInfo;
+
+public class PaymentSummaryActivity extends Activity {
+
+    private String referenceCallId;
+    private Button btEditPayment;
+    private TextView tvPaymentSummary;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_summary);
+
+        btEditPayment = (Button) findViewById(R.id.bt_edit_visa_payment);
+        tvPaymentSummary = (TextView) findViewById((R.id.tv_visa_payment_summary));
+
+        showVisaPaymentSummary();
+    }
+
+    public void showVisaPaymentSummary() {
+        String paymentSummaryMessage = getIntent().getExtras().getString(VISA_CHECKOUT_RESULT_MSG_KEY);
+
+        VisaPaymentSummary paymentSummary = getIntent().getExtras().getParcelable(VisaLibrary.PAYMENT_SUMMARY);
+        if (paymentSummary != null) {
+            paymentSummaryMessage +=
+                        "\nTotal Amount: " + getSampleVisaPaymentInfo().getTotal()
+                        +"\nTable Number: " + MainActivity.tableNo
+                        + "\nLast Four Digits : " + paymentSummary.getLastFourDigits()
+                        + "\nCard Brand : " + paymentSummary.getCardBrand();
+
+
+
+            referenceCallId = paymentSummary.getCallId();
+            if (!TextUtils.isEmpty(referenceCallId)) {
+                // pass the call Id back to relaunched payment
+                paymentSummaryMessage += "\nReference Call Id : " + referenceCallId;
+                btEditPayment.setText(getString(R.string.bt_edit_visa_payment_text));
+            }
+        }
+        tvPaymentSummary.setText(paymentSummaryMessage);
+    }
+
+    @Override
+    public void onBackPressed() {
+        backToPaymentStart(false);
+    }
+
+    public void editPayment(View v) {
+        backToPaymentStart(true);
+    }
+
+    private void backToPaymentStart(boolean launchInEditPaymentMode) {
+        // we are done with this activity
+        finish();
+
+        // start the payment activity again
+        Intent intent = new Intent(this, PaymentStartActivity.class);
+        if (launchInEditPaymentMode && !TextUtils.isEmpty(referenceCallId)) {
+            intent.putExtra(VISA_CHECKOUT_REFERENCE_ID_KEY, referenceCallId);
+        }
+        intent.putExtra("buttonType", getIntent().getExtras().getInt("buttonType"));
+        startActivity(intent);
+    }
+}
+
